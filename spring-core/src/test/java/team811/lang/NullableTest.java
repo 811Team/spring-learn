6d55019a48eb20313d7b151694a44e4793b50b6b
zhouxu <01153044@yto.net.cn>
2017-07-03 17:31:34 +0800

    提交

---
+++ spring-core/src/test/java/team811/lang/NullableTest.java
@@ -0,0 +1,18 @@
+package team811.lang;
+
+import org.junit.Test;
+
+/**
+ * @create: 2017-07-03
+ * @description: 理解 @Nullable 用途
+ */
+public class NullableTest {
+
+    public static void nullableCase(@Nullable String arg){
+
+    }
+
+    public static void main(String[] args) {
+        nullableCase(null);
+    }
+}
