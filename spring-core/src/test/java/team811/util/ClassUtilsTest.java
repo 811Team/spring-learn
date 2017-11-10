a44c0d2cd717c89b06f693726f2553fc51294058
zhouxu <01153044@yto.net.cn>
2017-11-10 17:55:46 +0800

    提交

---
+++ spring-core/src/test/java/team811/util/ClassUtilsTest.java
@@ -0,0 +1,15 @@
+package team811.util;
+
+import org.junit.Test;
+
+/**
+ * @create: 2017-11-10
+ * @description:
+ */
+public class ClassUtilsTest {
+
+    @Test
+    public void getShortNameTest() {
+        System.out.println(ClassUtils.getShortName(ClassUtils.class));
+    }
+}
