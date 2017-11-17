900d36a5816b703c197bda2cccbe27cfc307502c
zhouxu <01153044@yto.net.cn>
2017-11-17 17:25:37 +0800

    提交

--- spring-core/src/main/java/team811/core/task/SimpleAsyncTaskExecutor.java
+++ spring-core/src/main/java/team811/core/task/SimpleAsyncTaskExecutor.java
@@ -2,6 +2,7 @@ package team811.core.task;
 
 import team811.lang.Nullable;
 import team811.util.Assert;
+import team811.util.ConcurrencyThrottleSupport;
 import team811.util.CustomizableThreadCreator;
 import team811.util.concurrent.ListenableFuture;
 
@@ -22,21 +23,27 @@ public class SimpleAsyncTaskExecutor extends CustomizableThreadCreator
     @Nullable
     private TaskDecorator taskDecorator;
 
-    @Override
-    public ListenableFuture<?> submitListenable(Runnable task) {
-        return null;
-    }
+    /**
+     * 并发量控制对象
+     */
+    private final ConcurrencyThrottleAdapter concurrencyThrottle = new ConcurrencyThrottleAdapter();
 
-    @Override
-    public <T> ListenableFuture<T> submitListenable(Callable<T> task) {
-        return null;
-    }
-
+    /**
+     * 执行线程任务
+     *
+     * @param task         Runnable任务对象
+     * @param startTimeout
+     */
     @Override
     public void execute(Runnable task, long startTimeout) {
         Assert.notNull(task, "Runnable must not be null");
         // 判断是否存在任务装饰对象,如果有,则将任务放入任务装饰对象中执行
         Runnable taskToUse = (this.taskDecorator != null ? this.taskDecorator.decorate(task) : task);
+        if (isThrottleActive() && startTimeout > TIMEOUT_IMMEDIATE) {
+
+        } else {
+
+        }
     }
 
     @Override
@@ -53,4 +60,40 @@ public class SimpleAsyncTaskExecutor extends CustomizableThreadCreator
     public void execute(Runnable task) {
 
     }
+
+    /**
+     * 判断设置的并发量是否大于0
+     *
+     * @return boolean
+     */
+    public final boolean isThrottleActive() {
+        return this.concurrencyThrottle.isThrottleActive();
+    }
+
+    /**
+     * 并发量控制对象
+     */
+    private static class ConcurrencyThrottleAdapter extends ConcurrencyThrottleSupport {
+
+        @Override
+        protected void beforeAccess() {
+            super.beforeAccess();
+        }
+
+        @Override
+        protected void afterAccess() {
+            super.afterAccess();
+        }
+    }
+
+    @Override
+    public ListenableFuture<?> submitListenable(Runnable task) {
+        return null;
+    }
+
+    @Override
+    public <T> ListenableFuture<T> submitListenable(Callable<T> task) {
+        return null;
+    }
+
 }
