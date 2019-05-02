f65556ddda3296f36e4a5c50fc1259620140dca7
shaw <635776247@qq.com>
2019-05-03 03:24:13 +0800

    Commit

---
+++ spring-boot/spring-webmvc/src/main/java/team811/servlet/FrameworkServlet.java
@@ -0,0 +1,12 @@
+package team811.servlet;
+
+/**
+ * @author lihailen
+ * @date 2017-07-15
+ *
+ * 此源码来自己于Spring 4.1.5
+ *
+ * Spring Web 框架中的基础servlet.在一个基于JavaBean的整体解决方案中提供一个用于Spring应用上下文的集成。
+ */
+public abstract class FrameworkServlet {
+}
