83bb0a0fd377d001c6c13f2033e863e48305b6a7
shaw.zhou <shaw.zhou@winway666.com>
2019-05-20 19:11:30 +0800

    Commit

--- disruptor/src/main/java/org/shaw/RingBuffer.java
+++ disruptor/src/main/java/org/shaw/RingBuffer.java
@@ -16,12 +16,19 @@ abstract class RingBufferFields<E> extends RingBufferPad {
     private static final Unsafe UNSAFE = Util.getUnsafe();
 
     /**
-     * 指针偏移量
+     * 一个引用占用的字节数的幂次方
      */
     private static final int REF_ELEMENT_SHIFT;
 
+    /**
+     * 数组的开始地址，这里其实是数组中真正有效数据的开始地址，
+     * 是整个数组开始地址+BUFFER_PAD个引用的偏移量
+     */
     private static final long REF_ARRAY_BASE;
 
+    /**
+     * 数组中一共需要填充的个数
+     */
     private static final int BUFFER_PAD;
 
     static {
@@ -38,6 +45,14 @@ abstract class RingBufferFields<E> extends RingBufferPad {
         }
         BUFFER_PAD = 128 / scale;
         // Object[] 的基本偏移量
+        /**
+         *  等于 UNSAFE.arrayBaseOffset(Object[].class) + (BUFFER_PAD << REF_ELEMENT_SHIFT);
+         *
+         *  REF_ARRAY_BASE是整个数组的起始地址+用于缓存行填充的那些空位的偏移量
+         *  BUFFER_PAD << REF_ELEMENT_SHIFT表示BUFFER_PAD个引用的占用字节数
+         *  比如一个引用占用字节数是4，那么REF_ELEMENT_SHIFT是2，
+         *  BUFFER_PAD << REF_ELEMENT_SHIFT就相当于BUFFER_PAD * 4
+         */
         REF_ARRAY_BASE = UNSAFE.arrayBaseOffset(Object[].class) + 128;
     }
 
@@ -47,5 +62,8 @@ abstract class RingBufferFields<E> extends RingBufferPad {
 }
 
 abstract class RingBufferPad {
+    /**
+     * 缓存行左填充.
+     */
     protected long p1, p2, p3, p4, p5, p6, p7;
 }
--- disruptor/src/main/java/org/shaw/Sequence.java
+++ disruptor/src/main/java/org/shaw/Sequence.java
@@ -14,8 +14,14 @@ public class Sequence extends RhsPadding {
      */
     static final long INITIAL_VALUE = -1L;
 
+    /**
+     * {@code Unsafe} 对象
+     */
     private static final Unsafe UNSAFE;
 
+    /**
+     * {@link Value#value} 偏移量
+     */
     private static final long VALUE_OFFSET;
 
     static {
@@ -33,18 +39,32 @@ public class Sequence extends RhsPadding {
     }
 
     public Sequence(final long initialValue) {
+        // 插入StoreStore内存屏障,保证之前写入的数据对其它操作内存可见性.
         UNSAFE.putOrderedLong(this, VALUE_OFFSET, initialValue);
     }
 }
 
+/**
+ * 填充CPU缓存行,避免多线程修改互相独立的变量时，如果这些变量共享同一个缓存行，就会无意中影响彼此的性能.
+ * 64字节的缓存行中,前后个7个填充,保证数据在任何被切割情况下都不会被影响.
+ */
 class RhsPadding extends Value {
+    /**
+     * 右填充
+     */
     protected long p9, p10, p11, p12, p13, p14, p15;
 }
 
 class Value extends LhsPadding {
+    /**
+     * 保证该值的内存可见性
+     */
     protected volatile long value;
 }
 
 class LhsPadding {
+    /**
+     * 左填充
+     */
     protected long p1, p2, p3, p4, p5, p6, p7;
 }
