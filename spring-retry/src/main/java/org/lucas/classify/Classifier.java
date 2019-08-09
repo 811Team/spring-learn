3a1988e5827c320ad37746fb949794dc73f9871f
zhouxu <zhouxu@wljs.so>
2019-08-09 19:11:48 +0800

    Commit

--- spring-retry/src/main/java/org/lucas/classify/Classifier.java
+++ spring-retry/src/main/java/org/lucas/classify/Classifier.java
@@ -4,6 +4,12 @@ import java.io.Serializable;
 
 public interface Classifier<C, T> extends Serializable {
 
+    /**
+     * 通过类型进行匹配类型
+     *
+     * @param classifiable 匹配类型
+     * @return 被匹配到的类型
+     */
     T classify(C classifiable);
 
 }
--- spring-retry/src/main/java/org/lucas/classify/SubclassClassifier.java
+++ spring-retry/src/main/java/org/lucas/classify/SubclassClassifier.java
@@ -7,7 +7,7 @@ import java.util.concurrent.ConcurrentMap;
 
 public class SubclassClassifier<T, C> implements Classifier<T, C> {
 
-    private ConcurrentMap<Class<? extends T>, C> classified = new ConcurrentHashMap<Class<? extends T>, C>();
+    private ConcurrentMap<Class<? extends T>, C> classified = new ConcurrentHashMap<>();
 
     private C defaultValue = null;
 
@@ -16,30 +16,57 @@ public class SubclassClassifier<T, C> implements Classifier<T, C> {
     }
 
     public SubclassClassifier(C defaultValue) {
-        this(new HashMap<Class<? extends T>, C>(), defaultValue);
+        this(new HashMap<>(), defaultValue);
     }
 
     public SubclassClassifier(Map<Class<? extends T>, C> typeMap, C defaultValue) {
         super();
-        this.classified = new ConcurrentHashMap<Class<? extends T>, C>(typeMap);
+        this.classified = new ConcurrentHashMap<>(typeMap);
         this.defaultValue = defaultValue;
     }
 
     @Override
     public C classify(T classifiable) {
         if (classifiable == null) {
+            // 默认值
             return this.defaultValue;
         }
 
         Class<? extends T> exceptionClass = (Class<? extends T>) classifiable.getClass();
+        // classified 中有该key直接返回
         if (this.classified.containsKey(exceptionClass)) {
             return this.classified.get(exceptionClass);
         }
 
         C value = null;
+        // 通过父类获取
         for (Class<?> cls = exceptionClass; !cls.equals(Object.class) && value == null; cls = cls.getSuperclass()) {
             value = this.classified.get(cls);
         }
+
+        if (value == null) {
+            // 通过接口获取，包含父类接口
+            for (Class<?> cls = exceptionClass; !cls.equals(Object.class) && value == null; cls = cls.getSuperclass()) {
+                for (Class<?> ifc : cls.getInterfaces()) {
+                    value = this.classified.get(ifc);
+                    if (value != null) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (value != null) {
+            // 缓存
+            this.classified.put(exceptionClass, value);
+        }
+
+        if (value == null) {
+            // 还未找到则返回默认值
+            value = this.defaultValue;
+        }
+
+        return value;
     }
 
 }
