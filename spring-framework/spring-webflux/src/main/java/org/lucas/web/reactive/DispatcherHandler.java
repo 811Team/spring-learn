78db5ec2dc0533c899bc2cac2192fc146d47571f
zhouxu <zhouxu@wljs.com>
2020-08-11 10:35:05 +0800

    commit

--- spring-framework/spring-webflux/src/main/java/org/lucas/web/reactive/DispatcherHandler.java
+++ spring-framework/spring-webflux/src/main/java/org/lucas/web/reactive/DispatcherHandler.java
@@ -12,11 +12,18 @@ public class DispatcherHandler implements WebHandler, ApplicationContextAware {
         if (this.handlerMappings == null) {
             return createNotFoundError();
         }
+        // 1 查找对应的 controller 进行处理
+        // 1.1 获取所有的处理器映射
         return Flux.fromIterable(this.handlerMappings)
+                // 1.2 转换映射，获取处理器
                 .concatMap(mapping -> mapping.getHandler(exchange))
+                // 1.3 获取第一个处理器
                 .next()
+                // 1.4 如果不存在处理器，则创建一个错误信息作为元素
                 .switchIfEmpty(createNotFoundError())
+                // 1.5 如果有处理器，则调用 invokeHandler 进行处理
                 .flatMap(handler -> invokeHandler(exchange, handler))
+                // 1.6 处理处理器处理的结果，交由 handleResult 写回响应对象。
                 .flatMap(result -> handleResult(exchange, result));
     }
 }
